6f524d8da6155fdd05b26263bd27afd16650163c
Văn An <89497878+AnVan02@users.noreply.github.com>
2024-09-08 16:43:23 +0700

    Update bai1.java

--- bai1.java
+++ bai1.java
@@ -2,10 +2,10 @@ public class bai1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Ho ten: Văn Thi Hoài An");
+		System.out.println("Ho ten: Văn An");
 		System.out.println("Ma so sinh vien: 501200430");
 		System.out.println("Lop: CD20CT3");
-		System.out.print("So dien thoai: 0799472116 \tGioi Tinh: Nữ");
+		System.out.print("So dien thoai: 0123456789 \tGioi Tinh: Nữ");
 	}
 
 }
